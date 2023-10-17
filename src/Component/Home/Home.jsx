4586c53602a8e253bc0b90155761d16157fc84d0
SumiAkter2 <akteralisha36@gmail.com>
2023-10-18 01:08:08 +0600

    remove etrathing

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,18 +1,12 @@
 import Banner from "./Banner";
-// import DoorDash from "./DoorDash";
-// import Enterprise from "./Enterprise";
-// import Experience from "./Experience";
+
 import ManageSpend from "./ManageSpend/ManageSpend";
-// import SeatGeek from "./SeatGeek";
-// import SuperHuman from "./SuperHuman";
-// import WhatCan from "./WhatCan";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <ManageSpend />
-      
     </div>
   );
 };
--- src/Component/info/Info.jsx
+++ src/Component/info/Info.jsx
@@ -2,17 +2,12 @@ import { BsFillClockFill } from "react-icons/bs";
 import { RiMoneyDollarBoxFill } from "react-icons/ri";
 import { MdFindInPage } from "react-icons/md";
 import { MdHighQuality } from "react-icons/md";
-import { AiOutlineSafety } from "react-icons/ai";
-import { PiChatTextLight } from "react-icons/pi";
-import { LiaPuzzlePieceSolid } from "react-icons/lia";
+
 import logo1 from "../../assets/dashboard/app.png";
 import logo2 from "../../assets/dashboard/bmw.png";
 import logo3 from "../../assets/dashboard/t.png";
 import logo4 from "../../assets/dashboard/gm.png";
 
-const safe = <AiOutlineSafety size="35px" />;
-const text = <PiChatTextLight size="35px" />;
-const puzzle = <LiaPuzzlePieceSolid size="35px" />;
 const clock = <BsFillClockFill size="30px" />;
 const dollar = <RiMoneyDollarBoxFill size="30px" />;
 const travel = <MdFindInPage size="30px" />;
@@ -41,61 +36,6 @@ export const manageSpeedInfo = [
     img: quality,
     name: "Ensure higher quality",
     des: "Don't worry; we never compromises on quality. Component IQ ensures that every alternative component it suggests meets the highest quality and specification standards.",
-  },
-];
-export const enterpriseInfo = [
-  {
-    id: 1,
-    img: safe,
-    name: "Secure",
-    des1: "SOC 2 Certification",
-    des2: "PCI DSS Compliant",
-    des3: "Multi-factor ID authentication",
-    des4: "24/7 global fraud monitoring",
-    des5: "Self-serve Security & Privacy Center",
-  },
-  {
-    id: 2,
-    img: puzzle,
-    name: "Integrated",
-    des1: "ERP and accounting",
-    des2: "Productivity and HRIS",
-    des3: "API automations",
-    des4: "Security and dedicated SSO",
-    des5: "SSO for Enterprise IdP",
-  },
-  {
-    id: 3,
-    img: text,
-    name: "Hands-on",
-    des1: "Dedicated account specialist",
-    des2: "Dedicated customer success manager",
-    des3: "Implementation and training support",
-    des4: "24/7 customer service",
-    des5: "24/7 travel agent support",
-  },
-];
-
-export const geekInfo = [
-  {
-    id: 1,
-    img: "https://brand.Brex.com/transform/b2b42294-a96e-4939-9fa0-7d593a250924/Time_Logo?io=transform:fit,width:1400,height:432&quality=95",
-    name: "TIME named AUTOBOM AI to the TIME100 Most Influential Companies list for making an extraordinary impact around the world.",
-  },
-  {
-    id: 2,
-    img: "https://brand.Brex.com/transform/55dad09c-05d9-41fa-8cbf-2815bac732b5/Bloomberg_Logo?io=transform:fit,width:1400,height:261&quality=95",
-    name: "Bloomberg interviews AUTOBOM AI Co-CEO Henrique Dubugras on AI-driven spend management and banking innovation. ",
-  },
-  {
-    id: 3,
-    img: "https://brand.Brex.com/transform/1d336d33-240f-444d-87ee-38b0569ae177/Fast_Company_logo?io=transform:fit,width:1400,height:199&quality=95",
-    name: "Fast Company covers AUTOBOM AI's expansion from corporate cards for startups to enterprise expense management software.",
-  },
-  {
-    id: 4,
-    img: "https://brand.Brex.com/transform/f1e39cdd-e724-4a14-8b6f-f61958407cfb/CNBC?io=transform:fit,width:1400,height:203&quality=95",
-    name: "AUTOBOM AI named #2 on the 2023 top 50 disruptors list of innovative companies advancing breakthrough technology.",
   },
 ];
 
